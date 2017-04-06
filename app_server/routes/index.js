21003f57dd098f609b090fb531d2ac920bc86278
ahmedelshehe <ahmed.el-shehy@student.guc.edu.eg>
2017-04-06 03:18:11 +0300

    Update index.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,2 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var auth = jwt({
+  secret: 'secret',
+  userProperty: 'payload'
+});
+var ctrlAuth = require('../controller/authentication');
+router.post('/register', ctrlAuth.register);
+router.post('/login', ctrlAuth.login);
+module.exports = router;
