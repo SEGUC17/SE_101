62a062554e5aab264125fc9a867cf6d52ce53f1a
ahmedelshehe <ahmed.el-shehy@student.guc.edu.eg>
2017-04-06 21:28:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var flash   = require('connect-flash');
+var session=require('express-session');
 
-var index = require('./routes/index');
+var mongoose=require('mongoose');
+var index = require('./app_api/routes/index');
 var users = require('./routes/users');
+
 
 var app = express();
 
@@ -21,7 +25,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session({ secret: 'secret' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash());
 app.use('/', index);
 app.use('/users', users);
 
