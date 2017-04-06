a9a4c48b7bcac5372f3a86cb420cb3eb7737d664
appple <appple@users.noreply.github.com>
2017-04-06 11:58:51 +0200

    Update User.js

--- app_server/model/User.js
+++ app_server/model/User.js
@@ -1,13 +1,14 @@
 var mongoose = require('mongoose');
 var jwt = require('jsonwebtoken');
 var pr = require('mongoose').model(profileSchema);
+var prod = require('mongoose').model(productSchema);
 
 var UserSchema = new mongoose.Schema({
 username : {type:String, required:true,unique:true},
 password : {type:String, required:true},
-user_basket : [mongoose.Schema.Types.Mixed],
+user_basket : [prod], //only products the user has selected but hasnt bought yet are in this list
 user_history : [String],
-profile : pr,
+profile : pr, // the plan is in the profile 
 admin:{type:boolean}
 } );
 UserSchema.methods.generateJwt=function() {
