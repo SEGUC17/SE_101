d968868c812d033d7e5b958b489a2e5510b560b1
ahmedelshehe <ahmed.el-shehy@student.guc.edu.eg>
2017-04-06 13:04:13 +0300

    Update index.js

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,2 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('express-jwt');
+var multiparty=require('multiparty');
+var multer=require('multer');
+var upload=multer({dest:'uploads/'});
+var fs=require('fs');
+var path=require('path');
+var auth = jwt({
+  secret: 'secret',
+  userProperty: 'payload'
+});//This will be used to pass token to profile
+
+var ctrlAuth = require('../controller/authentication');
+var ctrlProfile=requmodule.exports = router;ire('../controller/profileController');
+var ctrlUser=require('../controller/userController');
+var ctrlAdmin=require('../controller/adminController')
+// authentication
+router.post('/register', ctrlAuth.register);
+router.post('/login', ctrlAuth.login);
+router.get('/profile',ctrlProfile.viewProfile);
+router.post('/editProfile',ctrlProfile.editProfile);
+router.get('/selectPlan/:plan_id',ctrlUser.selectPlan);
+router.post('/addPlan',ctrlAdmin.addPlan);
+router.post('/addProduct',ctrlAdmin.addProduct);
+router.post('/addSponserAd',upload.single('img'),ctrlAdmin.addSponserAd);
+
