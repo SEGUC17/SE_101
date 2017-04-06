5964871da75ca38b20d000eb59fdf45d2c372359
ahmedelshehe <ahmed.el-shehy@student.guc.edu.eg>
2017-04-06 04:06:26 +0300

    Create sponserAd.js

---
+++ app_server/model/sponserAd.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
+
+var sponserAdSchema = new mongoose.Schema({
+name : String,
+details : String
+image:{data:Buffer,contentType:String}
+});
+
+mongoose.model('sponserAd' , sponserAdSchema);
