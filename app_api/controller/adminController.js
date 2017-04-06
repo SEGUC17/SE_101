d2f93e20722f0d43b337ea4b3a1ad920ad70a1d3
alihesham44 <alyhesham44@gmail.com>
2017-04-06 13:28:41 +0200

    Update adminController.js

--- app_api/controller/adminController.js
+++ app_api/controller/adminController.js
@@ -1,6 +1,13 @@
 var passport = require('passport');
 var mongoose = require('mongoose');
 var User=mongoose.model('User');
+var sponserAd=mongoose.model('sponserAd');
+var plan=mongoose.model('plan');
+var multiparty=require('multiparty');
+var multer=require('multer');
+var upload=multer({dest:'uploads/'});
+var fs=require('fs');
+var path=require('path');
 
 module.exports.viewUsers=function(req,res){
 	User.find(function(err,doc){
