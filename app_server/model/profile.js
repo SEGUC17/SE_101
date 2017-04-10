950b44749454904654fa245d3bd41cc1a081a7f9
ahmedelshehe <ahmed.el-shehy@student.guc.edu.eg>
2017-04-10 18:32:44 +0200

    Update profile.js

--- app_server/model/profile.js
+++ app_server/model/profile.js
@@ -1,13 +1,14 @@
 var mongoose = require('mongoose'),
     Schema=mongoose.Schema;
 var jwt = require('jsonwebtoken');
-var pr = require('mongoose').model(productSchema);
-var pl = require('mongoose').model(planSchema);
-
+  var Product=require('../model/product');
+  var Plan=require('../model/plan');
+  var sponserAd=require('../model/sponserAd');
+  
 var profileSchema = new mongoose.Schema({
 name : String,
 products : [{type: Schema.Types.ObjectId, ref: 'Product'}], //this is the bought history not the cart
 plan:{type: Schema.Types.ObjectId, ref: 'Plan'}  
 });
 
-mongoose.model('Profile' , profileSchema, 'Profiles');
+mongoose.model('Profile' , profileSchema);
