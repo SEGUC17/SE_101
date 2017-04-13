51ebabc272ce4b8915be57f7017fc11808449394
okdah <okdah@live.com>
2017-04-13 13:58:03 +0200

    new chat

--- app_server/controller/chatController.js
+++ app_server/controller/chatController.js
@@ -7,50 +7,50 @@ module.exports.userChat = function(req, res) {
     console.log(req.method, req.url);
     Chat.find({
         user: req.query.id
-    }, function(err, chat) {
-      if (err) {
-          res.send(err);
-      }else {
-        if (chat == null) {
+    }, function(err, c) {
+        if (!c.user) {
             User.find({ admin: true }, function(err, users) {
-                var chat = new Chat()
-                chat.user = req.query.id
-                chat.admin = users[0]._id
+                if (users != []) {
+                    var c = new Chat()
+                    c.user = req.query.id
+                    c.admin = users[0]._id
 
-                var msg = new Message()
-                msg.text = req.body.text
-                msg.user = req.query.id
-                chat.messages.push(msg)
-                msg.save(function(err) {
-                    if (err) {
-                        res.send(err);
-                    }
-                    chat.save(function(err) {
+                    var msg = new Message()
+                    msg.text = req.body.text
+                    msg.user = req.query.id
+                    c.messages = [msg]
+                    msg.save(function(err) {
                         if (err) {
-                            res.send(err);
+                            res.send("can't save msg ", err);
                         }
-                        res.json({ message: 'new Chat message sent to admin' });
+                        c.save(function(err, chat) {
+                            if (err) {
+                                res.send(err);
+                            }
+                            res.send(chat);
+                        });
                     });
-                });
+                } else {
+                    res.json({ message: 'cant find admin' });
+                }
             });
         } else {
             var msg = new Message()
             msg.text = req.body.text
             msg.user = req.query.id
-            chat.messages.push(msg)
+            c.messages = [].push(msg)
             msg.save(function(err) {
                 if (err) {
-                    res.send(err);
+                    res.send("can't save msg ", err);
                 }
-                chat.save(function(err) {
-                    if (err) {
-                        res.send(err);
-                    }
-                    res.json({ message: 'new Chat message sent to admin' });
-                });
+            });
+            c.save(function(err) {
+                if (err) {
+                    res.send("can't save chat ", err);
+                }
+                res.json({ message: 'new Chat message sent to admin' });
             });
         }
-      }
     })
 };
 module.exports.userChatget = function(req, res) {
@@ -70,11 +70,7 @@ module.exports.adminChat = function(req, res) {
         user: req.query.user,
         admin: req.query.admin
     }, function(err, chat) {
-      if (err) {
-          res.send(err);
-      }
-      else {
-        if (chat == null) {
+        if (!chat.user) {
             var chat = new Chat()
             chat.user = req.query.id
             chat.admin = req.query.admin
@@ -82,35 +78,35 @@ module.exports.adminChat = function(req, res) {
             var msg = new Message()
             msg.text = req.body.text
             msg.user = req.query.admin
-            chat.messages.push(msg)
+            chat.messages = [].push(msg)
             msg.save(function(err) {
                 if (err) {
                     res.send(err);
                 }
-                chat.save(function(err) {
+                chat.save(function(err, chat) {
                     if (err) {
                         res.send(err);
                     }
-                    res.json({ message: 'new Chat message sent from admin to user' });
+                    res.send(chat);
                 });
             });
         } else {
             var msg = new Message()
             msg.text = req.body.text
             msg.user = req.query.admin
-            chat.messages.push(msg)
+            chat.messages = [].push(msg)
             msg.save(function(err) {
                 if (err) {
                     res.send(err);
                 }
-                chat.save(function(err) {
-                    if (err) {
-                        res.send(err);
-                    }
-                    res.json({ message: 'new Chat message sent from admin to user' });
-                });
             });
-        }}
+            chat.save(function(err, chat) {
+                if (err) {
+                    res.send(err);
+                }
+                res.send(chat);
+            });
+        }
     })
 };
 
