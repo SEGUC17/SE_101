fcf26ae4f55617c603d829887d477065dc29c576
appple <appple@users.noreply.github.com>
2017-04-18 20:41:42 +0200

    Update index.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -17,6 +17,7 @@ var ctrlProfile=require('../controller/profileController');
 var ctrlUser=require('../controller/userController');
 var ctrlAdmin=require('../controller/adminController');
 var ctrlProduct=require('../controller/productController');
+var ctrlChat = require('../controller/chatController');
 // authentication
 router.post('/register', ctrlAuth.register);
 router.post('/login', ctrlAuth.login);
@@ -26,18 +27,31 @@ router.get('/dashboard', passport.authenticate('jwt', { session: false }), funct
 
 router.get('/profile', passport.authenticate('jwt', { session: false }),ctrlProfile.viewProfile);
 router.post('/editProfile', passport.authenticate('jwt', { session: false }),ctrlProfile.editProfile);
-router.get('/selectPlan/:plan_id',passport.authenticate('jwt', { session: false }),ctrlUser.selectPlan);
+router.post('/selectPlan',passport.authenticate('jwt', { session: false }),ctrlUser.selectPlan);
 router.post('/addPlan',passport.authenticate('jwt', { session: false }),ctrlAdmin.addPlan);
 router.post('/addProduct',passport.authenticate('jwt', { session: false }),ctrlAdmin.addProduct);
 router.put('/editProduct',passport.authenticate('jwt', { session: false }),ctrlAdmin.editProduct);
+router.put('/editPlan',passport.authenticate('jwt', { session: false }),ctrlAdmin.editPlan);
 router.post('/addSponserAd',passport.authenticate('jwt', { session: false }),upload.single('img'),ctrlAdmin.addSponserAd);
+router.put('/editSponserAd',passport.authenticate('jwt', { session: false }),upload.single('img'),ctrlAdmin.editSponserAd);
+router.delete('/deletePlan',passport.authenticate('jwt', { session: false }),ctrlAdmin.deletePlan);
+router.delete('/deleteProduct',passport.authenticate('jwt', { session: false }),ctrlAdmin.deleteProduct);
+router.delete('/deleteSponserAd',passport.authenticate('jwt', { session: false }),ctrlAdmin.deleteSponserAd);
+router.delete('/deleteUser',passport.authenticate('jwt', { session: false }),ctrlAdmin.deleteUser);
 //router.get('/products',ctrlProduct.getProducts);
-router.put('/products',passport.authenticate('jwt', { session: false }),ctrlProduct.addProduct);
+router.post('/deleteUser',passport.authenticate('jwt', { session: false }),ctrlAdmin.deleteUser);
+router.get('/viewUsers',passport.authenticate('jwt', { session: false }),ctrlAdmin.viewUsers);
+router.post('/addproducts',passport.authenticate('jwt', { session: false }),ctrlProduct.addProduct);
 router.get('/products/cart',passport.authenticate('jwt', { session: false }),ctrlProduct.viewCart);
 router.put('/products/cart',passport.authenticate('jwt', { session: false }),ctrlProduct.removeFromCart);
 router.all('/products/invoice',passport.authenticate('jwt', { session: false }), ctrlProduct.checkout);
 router.get('profile/viewHistory',passport.authenticate('jwt', { session: false }), ctrlUser.viewHistory);
 router.get('/products',ctrlUser.viewProducts);
 router.get('/plans',ctrlUser.viewPlan);
+router.post('/user/chat:id', ctrlChat.userChat);
+router.get('/user/chat:id', ctrlChat.userChatget);
+router.post('/admin/chat:id:admin', ctrlChat.adminChat)
+router.get('/admin/chat:id:admin', ctrlChat.adminChatget)
+router.get('/admin/chat:admin', ctrlChat.adminChatgetAll);
 
 module.exports =router;
