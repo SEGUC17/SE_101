7d86de1a00371932097eea0ff7d3d8759e08a1e0
ahmedelshehe <ahmed.el-shehy@student.guc.edu.eg>
2017-04-20 19:47:23 +0300

    Add files via upload

---
+++ View/controllers/checkout.js
@@ -0,0 +1,16 @@
+angular.module('fitnessApp').
+  component('checkOut', {
+    templateUrl: 'components/checkout/checkout.html',
+    controller: function checkoutController($http) {
+      var self = this;
+      this.heading = "Cart";
+
+
+      $http.get('https://localhost:3000//products/cart').then(function(response) {
+        self.products = response.cart;
+        self.total=response.total;
+        
+      });
+
+        }
+  });
