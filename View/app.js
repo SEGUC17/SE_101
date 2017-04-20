0358418b640a5eb5734807b0d69edf234a6b911b
ahmedelshehe <ahmed.el-shehy@student.guc.edu.eg>
2017-04-21 01:56:36 +0200

    Update app.js

--- View/app.js
+++ View/app.js
@@ -2,6 +2,7 @@ var fitnessApp = angular.module('fitnessApp', ['ui.router']);
 //Each state url represent to the route 
 //when this route is called the template provided is viewed in index.html
 blogApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  //checkout state : <check-out> refers to checkout component
   var checkoutState = {
     name: 'checkout',
     url: '/charge',
