8ab7c0fb340d6ca64ab0606e714634c024cb3fcd
ahmedelshehe <ahmed.el-shehy@student.guc.edu.eg>
2017-04-21 01:54:26 +0200

    Update app.js

--- View/app.js
+++ View/app.js
@@ -1,1 +1,12 @@
-//application of the front end
+var fitnessApp = angular.module('fitnessApp', ['ui.router']);
+//Each state url represent to the route 
+//when this route is called the template provided is viewed in index.html
+blogApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  var checkoutState = {
+    name: 'checkout',
+    url: '/charge',
+    template: '<check-out></check-out>'
+  }
+  $stateProvider.state(postsState);
+  $urlRouterProvider.when('', '/products');
+}]);
