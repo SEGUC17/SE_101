99dedca3c0c9df17b3b1bf5f0d630122973eab12
appple <appple@users.noreply.github.com>
2017-04-21 09:42:31 +0200

    Update app.js

--- View/app.js
+++ View/app.js
@@ -1,5 +1,5 @@
-var fitnessApp = angular.module('fitnessApp', ['ui.router','chatController', 'checkoutController','chatSocket', 'socketService', 'btford.socket-io']);
-//Each state url represent to the route 
+var fitnessApp = angular.module('fitnessApp', ['ui.router','ngRoute','chatController', 'checkoutController','chatSocket', 'socketService', 'btford.socket-io']);
+//Each state url represent to the route
 //when this route is called the template provided is viewed in index.html
 blogApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
   //checkout state : <check-out> refers to checkout component
@@ -11,3 +11,21 @@ blogApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider,
   $stateProvider.state(checkOutState);
   $urlRouterProvider.when('', '/products');
 }]);
+
+fitnessApp.config(function($routeProvider){
+  
+  when('/charge',{
+    templateUrl : '/partials/checkout.html',
+    controller : 'checkoutController'
+  })
+
+  when('/admin' , {
+    templateUrl : '/partials/adminPage.html',
+    controller : 'adminPagectrl'
+  })
+
+  when('/admin/editProducts', {
+    templateUrl : '/partials/adminEditProducts.html',
+    controller : 'adminEditProductsctrl'
+  })
+});
