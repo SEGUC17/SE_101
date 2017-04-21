4db7abc53520d9292e6fe5561055f3decdb06cfc
appple <appple@users.noreply.github.com>
2017-04-21 17:31:29 +0200

    Update editProductsctrl.js

--- View/controllers/editProductsctrl.js
+++ View/controllers/editProductsctrl.js
@@ -1,18 +1,61 @@
 fitnessApp.controller('editproductsctrl', fuction($scope, editProductssrv){
-
+  var productName;
+  var productId;
+  var productDetails;
+  var productPrice;
   productssrv.getProductsList().success(function(productsList){
     $scope.products=productsList;
   });
 
-  $scope.CreateNewProduct =function(data){
+  $scope.CreateNewProduct =function(){
+    var data = {
+                      'name':productName,
+                      'details':productId,
+                      'id' :productDetails,
+                      'price':productPrice
+                  };
     editproductssrv.makeProduct(data);
   };
 
-  $scope.EditProduct = function(data){
+  $scope.EditProduct = function(){
+    var data = {
+                      'name':productName,
+                      'details':productId,
+                      'id' :productDetails,
+                      'price':productPrice
+                  };
     editproductssrv.editProduct(data);
   };
 
   $scope.DeleteProduct = function(data){
     deleteProduct(data);
-  }
+  };
+
+  $scope.ToNewProduct = function(){
+    productId = null;
+    productName = null;
+    productDetails = null;
+    productPrice = null;
+    $location.url('/admin/createNewProduct')
+  };
+
+  $scope.ToEditProduct = function(data){
+    productId=data;
+    productName = null;
+    productDetails = null;
+    productPrice = null;
+    $location.url('/admin/editProduct')
+  };
+
+  $scope.setProductName = function(name){
+    productName=name;
+  };
+
+  $scope.setProductPrice = function(price){
+    productPrice=price;
+  };
+
+  $scope.setProductDetails=function(detaisl){
+    productDetails=details;
+  };
 });
