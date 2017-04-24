414d16ac41e55fd2cdcdd2d8baedc1f527140132
appple <appple@users.noreply.github.com>
2017-04-24 23:57:51 +0200

    Update editProductssrv.js

--- View/services/editProductssrv.js
+++ View/services/editProductssrv.js
@@ -1,22 +1,22 @@
 fitnessApp('editproductssrv', function($http){
   return{
 
-
+//get a list of json products
     getProductsList : function(){
       return $http.get('/products');
     },
 
-
+//send product details to make new produce
     makeProduct : function(data){
       $http.post('/addProduct',data);
     },
 
-
+// take product id and attribute value to change
     editProduct : function(data){
       $http.put('/editProduct',data);
     },
     
-
+// take product id to delete from model
     deleteProduct : function(data){
       $http.delete('/deleteProduct',data);
     }
