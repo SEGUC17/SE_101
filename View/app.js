825a5f8d3841a90688aeec7de6fa084d76af41c9
ahmedelshehe <ahmed.el-shehy@student.guc.edu.eg>
2017-04-30 19:27:47 +0200

    Update app.js

--- View/app.js
+++ View/app.js
@@ -1,73 +1,9 @@
-
-var fitnessApp = angular.module('fitnessApp', ['ui.router','ngRoute','chatController','mainCtrl',
- 'authService', 'checkoutController','chatSocket', 'socketService', 'btford.socket-io', 'adminchatController', 'adminChatsctrl']);
-//Each state url represent to the route
-//when this route is called the template provided is viewed in index.html
-fitnessApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-  //checkout state : <check-out> refers to checkout component
-  var checkoutState = {
-    name: 'checkout',
-    url: '/checkout',
-    template: '/partials/checkout.html'
-  }
-  $stateProvider.state(checkOutState);
-  $urlRouterProvider.when('', '/products');
-}]);
-
-fitnessApp.config(function($routeProvider){
-  when('/login', {
-            templateUrl : '/partials/login.html',
-        	controller   : 'mainController',
-        	controllerAs   : 'login'
-        })
-  when('/', {
-            templateUrl : '/partials/home.html',
-            controller : 'homeCtrl',
-            controllerAs : 'home'
-        })
-
-  when('/checkout',{
-    templateUrl : '/partials/checkout.html',
-    controller : 'checkoutController'
-  })
-
-  when('/admin' , {
-    templateUrl : '/partials/adminPage.html',
-    controller : 'adminPagectrl'
-  })
-
-  when('/admin/editProducts', {
-    templateUrl : '/partials/adminEditProducts.html',
-    controller : 'editProductsctrl'
-  })
-
-
-  when('/admin/editPlans', {
-    templateUrl : '/partials/adminEditPlans.html',
-    controller : 'plansctrl'
-  })
-
-  when('/admin/editUsers', {
-    templateUrl : '/partials/adminEditUsers.html',
-    controller : 'editUsersctrl'
-  })
-
-  when('/admin/editSponserAds', {
-    templateUrl : '/partials/adminEditSponserAds.html',
-    controller : 'editSponserAdsctrl'
-  })
-
-  when('/admin/createNewProduct',{
-    templateUrl : '/partials/createNewProduct.html',
-    controller : 'editProductsctrl'
-  })
-
-    when('/admin/chats',{
-    templateUrl : '/partials/adminchats.html',
-    controller : 'adminChatsctrl'
-  })
-});
- fitnessApp.config(function($httpProvider){
+angular.module('fitnessApp', [ 
+	'app.routes',
+	'authService',
+	'mainCtrl',
+	])
+.config(function($httpProvider)	{	
 	//attach our auth inteceptor to the http requests
 	$httpProvider.interceptors.push('AuthInterceptor');
 });
