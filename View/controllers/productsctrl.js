50f280bdaf2a0bf5ed65ce4e4e3f2d30d12b8c9a
appple <appple@users.noreply.github.com>
2017-05-01 16:28:49 +0200

    Update productsctrl.js

--- View/controllers/productsctrl.js
+++ View/controllers/productsctrl.js
@@ -1,10 +1,40 @@
 //returns the list of products
 
-fitnessApp.controller('productsctrl', fuction($scope, productssrv){
 
-  productssrv.getProductsList().success(function(productsList){
-    $scope.products=productsList;
+angular.module('productsctrl' , [])
+  .controller('productsctrl' , function($http,$scope, productssrv){
+
+ $scope.products= productssrv.getResult();
+//$scope.products = [{name: "Test" , details: "bla bla" , price: 2.0}, {name: "Test1" , details: "bla bla" , price: 2.0}];
+
+$scope.getProducts = function(){
+  productssrv.getProductsList().then(function(response){
+
+    productssrv.setResult(response.data);
+
+    console.log(response.data);
+    console.log(productssrv.getResult);
   });
+}
 
 
-});
+  })
+
+  .factory('productssrv', function($http){
+    var result;
+      return{
+
+    //get a list of json products
+        getProductsList : function(){
+          return $http.get('/products');
+        },
+
+        getResult : function() {
+          return this.result;
+        },
+
+        setResult : function(value) {
+          this.result=value;
+        }
+      };
+    });
