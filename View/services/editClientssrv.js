eed4941879741c3e14e9690e92384d4f28352d2a
alihesham44 <alyhesham44@gmail.com>
2017-05-03 15:58:11 +0200

    Update editClientssrv.js

--- View/services/editClientssrv.js
+++ View/services/editClientssrv.js
@@ -1,7 +1,22 @@
 //connects to the back end and gets the users that wants to be viewed or deleted
 
 fitnessApp('editClientssrv', function($http){
-  return{
+ 
+  var list = [];
+    return{
+
+      setList : function(value) {
+        this.list=value;
+      },
+
+      getList : function() {
+        return this.list;
+        },
+  //get a list of json clients
+      getUserssList : function(){
+        return $http.get('/viewUsers');
+      },
+
    
     getClientsList : function(){
       return $http.get('/viewUsers');
