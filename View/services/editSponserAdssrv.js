b014d42b2aefd93ce64040abe6770fbdd8fbf7b1
alihesham44 <alyhesham44@gmail.com>
2017-05-03 16:05:28 +0200

    Update editSponserAdssrv.js

--- View/services/editSponserAdssrv.js
+++ View/services/editSponserAdssrv.js
@@ -1,7 +1,20 @@
 // connects to the back end to get ads which are needed to be deleted or edited, also adds the new ads into the back end 
 
 fitnessApp('editSponserAdssrv', function($http){
+  var List[];
   return{
+    
+      setList : function(value) {
+        this.list=value;
+      },
+
+      getList : function() {
+        return this.list;
+        },
+  //get a list of json ADS
+      getAdsList : function(){
+        return $http.get('/viewSponserAd');
+      },
     
         makeSponserAd : function(data){
       $http.post('/addSponserAd',data);
