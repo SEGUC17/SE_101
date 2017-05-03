0b803ca0634ebbf523a46e062ae34f208de1c687
alihesham44 <alyhesham44@gmail.com>
2017-05-03 16:02:13 +0200

    Update editClientsctrl.js

--- View/controllers/editClientsctrl.js
+++ View/controllers/editClientsctrl.js
@@ -1,9 +1,12 @@
 //here  you can view or delete
 fitnessApp.controller('editClientsctrl', fuction($scope, editClientssrv){
+                        editClientsssrv.getUsersList().success(function(UsersList){
+    $scope.Users=UsersList;
+  });
 
 
-  editClientssrv.getClientsList().success(function(clientsList){
-    $scope.clients=clientsList;
+  editClientssrv.getClientsList().success(function(UsersList){
+    $scope.clients=UsersList;
   });
 
   $scope.DeleteClients = function(data){
